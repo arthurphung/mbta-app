c053f37452388d87f77b74ef7bdcc6292e9e47c5
arthurphung <rthrphung73@gmail.com>
2023-01-04 11:07:04 -0500

    working stream close logic

--- pages/tracking/[stopId].tsx
+++ pages/tracking/[stopId].tsx
@@ -1,24 +1,43 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Tracking() {
     const router = useRouter();
     const stopId = router.query.stopId;
     console.log(stopId);
 
-    const predictionsStream = new EventSource(`/api/predictions/${stopId}`);
-    const events: Array<string> = ['reset', 'add', 'update', 'remove'];
+    useEffect(() => {
+        const predictionsStream = new EventSource(`/api/predictions/${stopId}`);
+        const events: Array<string> = ['reset', 'add', 'update', 'remove'];
+    
+        for (const event of events) {
+            predictionsStream.addEventListener(event, (e) => {
+                console.log(event, JSON.parse(e.data));
+            });
+        }
+    
+        predictionsStream.onopen = () => {
+            console.log('connection to stream has been opened');
+        };
+    
+        predictionsStream.onerror = (error) => {
+            console.log('an error occurred while receiving stream', error);
+        };
+    
+        predictionsStream.addEventListener('close', () => {
+            predictionsStream.close();
+        })
 
-    for (const event of events) {
-        predictionsStream.addEventListener(event, (e) => {
-            console.log(event, JSON.parse(e.data));
-        });
-    }
+        const handleRouteChange = (url: string, { shallow }: { shallow:boolean }) => {
+            console.log(`App is changing to ${url} ${shallow ? 'with' : 'without'} shallow routing`);
+        }
 
-    predictionsStream.onopen = () => {
-        console.log('connection to stream has been opened');
-    };
+        router.events.on('routeChangeStart', handleRouteChange);
 
-    predictionsStream.onerror = (error) => {
-        console.log('an error occurred while receiving stream', error);
-    };
+        return () => {
+            router.events.off('routeChangeStart', handleRouteChange);
+            predictionsStream.close();
+            console.log('connection closed');
+        }
+    }, []);
 };
